31d03b393763c68473c686e630bb52fea6ec141e
soko Awen <havenhamelin@gmail.com>
2024-02-25 21:31:13 -0500

    fleshed out dialogue system

--- Project/Entities/NPC/npc.gd
+++ Project/Entities/NPC/npc.gd
@@ -3,11 +3,9 @@ extends CharacterBody3D
 func _ready():
 	pass
 
-func _process(_delta):
-	#var camera_pos = get_viewport().get_camera_3d().global_transform.origin
-	#look_at(Vector3(camera_pos.x,0,camera_pos.z), Vector3(0, 1, 0))
-	pass
-
-func _on_interact_area_body_entered(body):
-	if body is player:
-		DialogueManager.show_dialogue_balloon(load("res://Entities/NPC/TestPenguin/test.dialogue"), "test")
+func _physics_process(_delta):
+	var camera_pos = get_viewport().get_camera_3d().global_transform.origin
+	var camera_distance = Vector2(position.x,position.z).distance_to(Vector2(camera_pos.x,camera_pos.z))
+	if camera_distance > 1:
+		look_at(Vector3(camera_pos.x,position.y,camera_pos.z))
+	
--- Project/Entities/Player/player.gd
+++ Project/Entities/Player/player.gd
@@ -7,25 +7,32 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+func _ready():
+	GameController.movement = true
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("interact"):
+		var actionables = $ActionableFinder.get_overlapping_areas()
+		if actionables.size() > 0:
+			actionables[0].action()
+			return
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	if GameController.movement == true:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity.y -= gravity * delta
+		# Handle jump.
+		if Input.is_action_just_pressed("move_jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		# Get the input direction and handle the movement/deceleration.
+		var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+		var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+		move_and_slide()
