6167a88e92ab1dfc47174fb3be62199cb3294c69
soko Awen <havenhamelin@gmail.com>
2024-03-14 14:50:09 -0400

    a bunch of stuff

--- Project/menus/pause_menu/pause_menu.gd
+++ Project/menus/pause_menu/pause_menu.gd
@@ -1,14 +1,17 @@
 extends CanvasLayer
+var unpause_cooldown = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Control/Unpause.grab_focus()
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_released("pause") and unpause_cooldown == false:
+		unpause_cooldown = true
+	if unpause_cooldown == true and Input.is_action_pressed("pause"):
+		_on_unpause_pressed()
 
 
 func _on_unpause_pressed():
