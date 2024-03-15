b536970a79b0eeebc15c6245bc3c95977b39c4c2
soko7awen <havenhamelin@gmail.com>
2024-03-15 13:49:41 -0400

    Revert "attempt merge conflict resolve"
    This reverts commit 109316fb563620a67ee527962723928d6b99829f, reversing
    changes made to de622c846d9370d3dbf25630b32c2000c72bf90e.

--- Project/globals/game_controller.gd
+++ Project/globals/game_controller.gd
@@ -7,11 +7,8 @@ func _process(_delta):
 	if state != null:
 		if state == 'riddle_success':
 			get_node('../main/'+stateNode).collision_layer = 0
-<<<<<<< HEAD
 		elif state == 'strength':
 			print(get_node('../main/'+stateNode))
-=======
->>>>>>> 44d49bf0a9a4ff4fa462d75780a6ef95e0d51a1b
 		state = null
 
 func set_player_movement(value: bool):
